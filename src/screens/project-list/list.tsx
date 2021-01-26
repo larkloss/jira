78f859719c885bb4879ee2a7bc2ad4cb790018be
Larkloss <dingzhao1995@gmail.com>
2021-01-26 18:32:24 +0800

    chore: 修改为tsx类型，提高安全性和稳定性

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import {User} from "./search-panel"
 
-export const List = ({list, users}) => {
+interface Project {
+    id:string,
+    name:string,
+    personId:string,
+    pin:boolean,
+    organization:string,
+}
+interface ListProps {
+    list: Project[],
+    users: User[],
+}
+export const List = ({list, users}:ListProps) => {
     return <table>
         <thead>
             <tr>
@@ -11,7 +23,7 @@ export const List = ({list, users}) => {
         <tbody>
             {
                 list.map(project =>
-                        <tr key={project}>
+                        <tr key={project.id}>
                             <td>{project.name}</td>
                             <td>{users.find(user => user.id === project.personId)?.name || '未知'}</td>
                         </tr>
