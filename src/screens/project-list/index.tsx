f4429f64a94de66d949755c160091cd605487fdc
Larkloss <dingzhao1995@gmail.com>
2021-02-17 19:08:36 +0800

    feat: 用useDocumentTitle提交文档标题

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { SearchPanel } from "screens/project-list/search-panel";
 import { List } from "screens/project-list/list";
-import { useDebounce } from "../../utils";
+import {useDebounce, useDocumentTitle} from "../../utils";
 import styled from "@emotion/styled";
 import { Typography } from "antd";
 import { useProjects } from "utils/project";
 import { useUsers } from "utils/user";
+import {Helmet} from "react-helmet";
 
 // 使用 JS 的同学，大部分的错误都是在 runtime(运行时) 的时候发现的
 // 我们希望，在静态代码中，就能找到其中的一些错误 -> 强类型
@@ -18,6 +19,7 @@ export const ProjectListScreen = () => {
     const { isLoading, error, data: list } = useProjects(debouncedParam);
     const { data: users } = useUsers();
 
+    useDocumentTitle('项目列表')
     return (
         <Container>
             <h1>项目列表</h1>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -31,3 +31,18 @@ export const useDebounce = <V>(value:V, delay?:number) => {
     }, [value, delay])
     return debounceValue
 }
+
+export const useDocumentTitle = (title: string, keepOnUnmount:boolean =  true) => {
+    const oldTitle = document.title
+    useEffect(() => {
+        document.title = title
+    }, [title])
+
+    useEffect(() => {
+        return () => {
+            if(!keepOnUnmount) {
+                document.title = oldTitle
+            }
+        }
+    }, [])
+}
