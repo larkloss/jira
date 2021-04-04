77eac31adcc129894fade4ac646aa81777be270a
Larkloss <dingzhao1995@gmail.com>
2021-04-05 04:30:52 +0800

    style: 优化项目样式

--- src/screens/kanban/index.tsx
+++ src/screens/kanban/index.tsx
@@ -5,13 +5,15 @@ import {useKanbanSearchParams, useProjectInUrl} from "./util";
 import {KanbanColumn} from "./kanban-column";
 import styled from "@emotion/styled";
 import {SearchPanel} from "./search-panel";
+import { ScreenContainer } from "components/lib";
 
 
 export const KanbanScreen = () => {
     useDocumentTitle('看板列表')
     const {data: currentProject} = useProjectInUrl()
     const {data: kanbans = []} = useKanbans(useKanbanSearchParams())
-    return <div>
+    return (<div>
+        <ScreenContainer>
         <h1>{currentProject?.name}看板</h1>
         <SearchPanel/>
         <ColumnsContainer>
@@ -21,11 +23,13 @@ export const KanbanScreen = () => {
                 )
             }
         </ColumnsContainer>
+        </ScreenContainer>
     </div>
+    )
 }
 
 const ColumnsContainer = styled.div `
   display: flex;
-  overflow: hidden;
-  margin-right: 2rem;
+  overflow-x: scroll;
 `
+
--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import { SearchPanel } from "screens/project-list/search-panel";
 import { List } from "screens/project-list/list";
-import {useDebounce} from "../../utils";
+import {useDebounce, useDocumentTitle} from "../../utils";
 import styled from "@emotion/styled";
 import { useProjects } from "utils/project";
 import { useUsers } from "utils/user";
 import {useUrlQueryParam} from "../../utils/url";
 import {useProjectModal, useProjectsSearchParams} from "./util";
-import {ButtonNoPadding, ErrorBox, Row} from "../../components/lib";
+import {ButtonNoPadding, ErrorBox, Row, ScreenContainer} from "../../components/lib";
 
 // 使用 JS ，大部分的错误都是在 runtime(运行时) 的时候发现的
 // 我们希望，在静态代码中，就能找到其中的一些错误 -> 强类型
 export const ProjectListScreen = () => {
+    useDocumentTitle("项目列表", false);
     const [param, setParam] = useProjectsSearchParams()
     const {open} = useProjectModal()
     const { isLoading, error, data: list } = useProjects(useDebounce(param, 200));
@@ -19,7 +20,7 @@ export const ProjectListScreen = () => {
 
     useUrlQueryParam([''])
     return (
-        <Container>
+        <ScreenContainer>
             <Row between={true}>
                 <h1>项目列表</h1>
                 <ButtonNoPadding onClick={open} type={"link"}>创建项目</ButtonNoPadding>
@@ -27,10 +28,6 @@ export const ProjectListScreen = () => {
             <SearchPanel users={users || []} param={param} setParam={setParam} />
             <ErrorBox error={error}/>
             <List  loading={isLoading} users={users || []} dataSource={list || []} />
-        </Container>
+        </ScreenContainer>
     );
 };
-
-const Container = styled.div`
-  padding: 3.2rem;
-`;
